d3376f521985a9d6b7bb9a7eac8f0f4e3758c3a3
Whylii <165547367+Whylii@users.noreply.github.com>
2026-05-21 16:40:15 +1200

    Test BW Summary Screen
    by ravepossum

--- src/evolution_scene.c
+++ src/evolution_scene.c
@@ -3,6 +3,7 @@
 #include "battle.h"
 #include "battle_message.h"
 #include "bg.h"
+#include "bw_summary_screen.h"
 #include "data.h"
 #include "decompress.h"
 #include "evolution_scene.h"
@@ -978,6 +979,13 @@ static void Task_EvolutionScene(u8 taskId)
             if (!gPaletteFade.active)
             {
                 FreeAllWindowBuffers();
+                if (BW_SUMMARY_SCREEN)
+                {
+                    ShowSelectMovePokemonSummaryScreen_BW(gPlayerParty, gTasks[taskId].tPartyId,
+                                CB2_EvolutionSceneLoadGraphics,
+                                gMoveToLearn);
+                }
+                else
                 ShowSelectMovePokemonSummaryScreen(gPlayerParty, gTasks[taskId].tPartyId,
                             CB2_EvolutionSceneLoadGraphics,
                             gMoveToLearn);
@@ -1369,6 +1377,15 @@ static void Task_TradeEvolutionScene(u8 taskId)
                 if (gTasks[taskId].tPartyId == PC_MON_CHOSEN)
                 {
                     ShowSelectMovePokemonSummaryScreen(
+                        if (BW_SUMMARY_SCREEN)
+                {
+                    ShowSelectMovePokemonSummaryScreen_BW(
+                            gEnemyParty, 
+                            TRADEMON_FROM_PC,
+                            CB2_TradeEvolutionSceneLoadGraphics,
+                            gMoveToLearn);
+                }
+                else
                                 gEnemyParty, 
                                 TRADEMON_FROM_PC,
                                 CB2_TradeEvolutionSceneLoadGraphics,
@@ -1378,6 +1395,15 @@ static void Task_TradeEvolutionScene(u8 taskId)
                 else
                 {
                     ShowSelectMovePokemonSummaryScreen(
+                        if (BW_SUMMARY_SCREEN)
+                {
+                    ShowSelectMovePokemonSummaryScreen_BW(
+                            gPlayerParty, 
+                            gTasks[taskId].tPartyId,
+                            CB2_TradeEvolutionSceneLoadGraphics,
+                            gMoveToLearn);
+                }
+                else
                                 gPlayerParty, 
                                 gTasks[taskId].tPartyId,
                                 CB2_TradeEvolutionSceneLoadGraphics,
