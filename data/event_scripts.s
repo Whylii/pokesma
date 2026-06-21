8c76c88bacb183a84f9bcfdeb8d6cad95393cb55
Whylii <whylias@proton.me>
2026-06-21 12:15:01 +1200

    Littleroot House 3 Gift and Flag

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1747,3 +1747,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/scripts/battle_frontier.inc"
 	.include "data/scripts/apricorn_tree.inc"
 	.include "data/scripts/wild_encounter.inc"
+
+	.include "data/maps/LittlerootTown_House3_1F/scripts.inc"
