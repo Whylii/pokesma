9cb515f1b83f89acb88c0d1719259ab8a026a71e
Whylii <whylias@proton.me>
2026-06-28 13:11:47 +1200

    Quest Menu
    Quest fixes

--- src/new_game.c
+++ src/new_game.c
@@ -49,6 +49,7 @@
 #include "mystery_gift.h"
 #include "union_room_chat.h"
 #include "constants/map_groups.h"
+#include "quests.h"
 #include "constants/items.h"
 #include "tx_registered_items_menu.h"
 #include "difficulty.h"
@@ -236,6 +237,7 @@ void NewGameInitData(void)
     ResetItemFlags();
     ResetDexNav();
     ClearFollowerNPCData();
+    QuestMenu_ResetMenuSaveData();
 }
 
 static void ResetMiniGamesRecords(void)
